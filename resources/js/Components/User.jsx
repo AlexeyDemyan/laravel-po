4c8e3cb9fe91fa99c3f1b092d864299f5005e99d
Alexey Demyanov <deal110988@him-mail.ch>
2024-11-07 17:09:17 +0100

    added styles to Users list

--- resources/js/Components/User.jsx
+++ resources/js/Components/User.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-export default function User({user}) {
-    console.log(user)
+export default function User({ user }) {
+    console.log(user);
 
     return (
-        <div>
-        <h2>{user.name}</h2>
+        <div className="p-6 flex space-x-2">
+            <div className="flex-1">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <span className="text-gray-800">{user.name}</span>
+                        <small className="ml-2 text-sm text-gray-600">
+                            Some data
+                        </small>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
