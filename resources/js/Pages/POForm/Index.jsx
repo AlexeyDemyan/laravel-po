ae37245af7ceefdfb72d05a25b43850e74e3ce1b
Alexey Demyanov <deal110988@him-mail.ch>
2024-11-18 15:44:27 +0100

    working on Form

--- resources/js/Pages/POForm/Index.jsx
+++ resources/js/Pages/POForm/Index.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import InputLabel from "@/Components/InputLabel";
+import TextInput from "@/Components/TextInput";
+import PrimaryButton from '@/Components/PrimaryButton';
 import { Head } from "@inertiajs/react";
 
 export default function Index() {
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        console.log('will post data here')
+    }
+
     return (
         <AuthenticatedLayout>
             <Head title="POForm" />
-
-            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-                <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
-                    PO Form will be here
+            <form onSubmit={submit} className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+                <div className="flex flex-row items-center">
+                    <InputLabel htmlFor="supplier" value="Supplier"/>
+                    <TextInput
+                    />
                 </div>
-            </div>
+            </form>
+
         </AuthenticatedLayout>
     );
 }
