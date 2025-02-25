2bd0a8c58435de44c87bc7b94285020c83b1728f
Alexey Demyanov <deal110988@him-mail.ch>
2025-02-25 13:27:16 +0100

    finished and tested merge script

--- merge.php
+++ merge.php
@@ -1,58 +1,75 @@
 <?php
 
+// $json_string = file_get_contents("../one-order.json");
+$json_string = file_get_contents("../po-entries-backup.json");
+$json_decoded = json_decode($json_string);
+
 $db = new SQLite3('database\database.sqlite');
 
-// $results = $db->query('select * from g_r_n_entries');
+foreach ($json_decoded as $value) {
+    [
+        'company' => $company,
+        'date' => $date,
+        'supplier' => $supplier,
+        'supplierAddress' => $supplierAddress,
+        'supplierCode' => $supplierCode,
+        'deliveryDate' => $deliveryDate,
+        'orderLines' => $orderLines,
+        'paymentTerms' => $paymentTerms,
+        'otherRemarks' => $otherRemarks,
+        'discount' => $discount,
+        'netTotalValue' => $netTotalValue,
+        'priceIncludesVat' => $priceIncludesVat,
+        'createdAt' => $createdAt,
+        'updatedAt' => $updatedAt
+    ] = get_object_vars($value);
 
-// while ($row = $results->fetchArray()) {
-//     echo $row["remarks"];
-//     echo "\n";
-// }
+    if ($date) {
+        ['$date' => $date_to_string] = get_object_vars(($date));
+    } else {
+        $date_to_string = "";
+    };
 
-// $stmt = $db->exec("INSERT INTO p_o_entries (company, supplier,priceIncludesVat) VALUES ('PHP company', 'Supplier PHP', 'YES')");
-// $result = $stmt->execute();
+    if ($deliveryDate) {
+        ['$date' => $delivery_date_to_string] = get_object_vars(($deliveryDate));
+    } else {
+        $delivery_date_to_string = "";
+    }
 
-// var_dump($result->fetchArray());
+    if ($createdAt) {
+        ['$date' => $created_at_to_string] = get_object_vars(($createdAt));
+    } else {
+        $created_at_to_string = "";
+    }
 
-$company = 'Marsovin Viticulture Ltd';
-$date = '2024-05-22T00:00:00.000Z';
-$supplier = 'Some Sup';
-$supplierAddress = 'Triq Xismu';
-$supplierCode = '';
-$deliveryDate = '2024-05-22T00:00:00.000Z';
+    if ($updatedAt) {
+        ['$date' => $updated_at_to_string] = get_object_vars(($updatedAt));
+    } else {
+        $updated_at_to_string = "";
+    }
 
-$oneOrderLine = "{\"product\":\"Bolts\",\"supplierRef\":\"BLT010\",\"quantity\":\"6\",\"unitPrice\":\"30.99\",\"totalPrice\":\"\"}";
-//$orderLines = "[{\"product\":\"Bolts\",\"supplierRef\":\"BLT010\",\"quantity\":\"6\",\"unitPrice\":\"30.99\",\"totalPrice\":\"\"},{\"product\":\"Rivets\",\"supplierRef\":\"RVT999\",\"quantity\":\"13.55\",\"unitPrice\":\"69\",\"totalPrice\":\"\"}]";
+    $orderLinesAll = [];
 
-$product = 'Some prod';
-$supplierRef = '';
-$quantity = '5.55';
-$unitPrice = '50.5';
-$totalPrice = '69.69';
+    foreach ($orderLines as $value) {
+        [
+            "product" => $product,
+            "supplierRef" => $supplierRef,
+            "quantity" => $quantity,
+            "unitPrice" => $unitPrice,
+            "totalPrice" => $totalPrice
+        ] = get_object_vars($value);
 
-$orderLine = "{\"product\":\"{$product}\",\"supplierRef\":\"{$supplierRef}\",\"quantity\":\"{$quantity}\",\"unitPrice\":\"{$unitPrice}\",\"totalPrice\":\"{$totalPrice}\"}";
+        $orderLine = "{\"product\":\"{$product}\",\"supplierRef\":\"{$supplierRef}\",\"quantity\":\"{$quantity}\",\"unitPrice\":\"{$unitPrice}\",\"totalPrice\":\"{$totalPrice}\"}";
+        array_push($orderLinesAll, $orderLine);
+    }
 
-$orderLines = "[{$orderLine},{$oneOrderLine}]";
+    $orderLinesImploded = (implode(",", $orderLinesAll));
+    $orderLines = "[{$orderLinesImploded}]";
 
-$paymentTerms = '30 days';
-$otherRemarks = 'Consumables';
-$discount = null;
-$netTotalValue = 111.55;
-$priceIncludesVat = 'No';
-$createdAt = '2024-07-17T12:08:34.442Z';
-$updatedAt = '2024-07-17T12:08:34.442Z';
-$insert_string = "INSERT INTO p_o_entries (company, date, supplier, supplierAddress, supplierCode, deliveryDate, orderLines, paymentTerms, otherRemarks, discount, netTotalValue, priceIncludesVat, created_at, updated_at) VALUES ('{$company}', '{$date}', '{$supplier}', '{$supplierAddress}', '{$supplierCode}', '{$deliveryDate}', '{$orderLines}', '{$paymentTerms}', '{$otherRemarks}', '{$discount}', {$netTotalValue}, '{$priceIncludesVat}', '{$createdAt}', '{$updatedAt}')";
+    $insert_string = "INSERT INTO p_o_entries (company, date, supplier, supplierAddress, supplierCode, deliveryDate, orderLines, paymentTerms, otherRemarks, discount, netTotalValue, priceIncludesVat, created_at, updated_at) VALUES ('{$company}', '{$date_to_string}', '{$supplier}', '{$supplierAddress}', '{$supplierCode}', '{$delivery_date_to_string}', '{$orderLines}', '{$paymentTerms}', '{$otherRemarks}', '{$discount}', '{$netTotalValue}', '{$priceIncludesVat}', '{$created_at_to_string}', '{$updated_at_to_string}')";
 
-$db->exec($insert_string);
+    // var_dump($insert_string);
+    // echo "\n";
 
-// $stmt = $db->prepare("INSERT INTO p_o_entries (company, date, supplier,supplierAddress, supplierCode, deliveryDate, paymentTerms, otherRemarks, discount, netTotalValue, priceIncludesVat, created_at, updated_at) VALUES ('?', '2024-05-22T00:00:00.000Z', 'Orttt', 'Triq Qormi', '', '2024-05-22T00:00:00.000Z', '30 days', 'Consumables', null, 575.39, 'No', '2024-07-17T12:08:34.442Z', '2024-07-17T12:08:34.442Z')", ($company));
-// $stmt->bindValue(':company', $company);
-// $stmt->execute();
-
-// $name = "Maria";
-// $info["last_name"] = "Warner";
-
-// echo "Hello {$name} {$info["last_name"]}";
-
-// (company, date, supplier,supplierAddress, supplierCode, deliveryDate, paymentTerms, otherRemarks, discount, netTotalValue, priceIncludesVat, createdAt, updatedAt)
-// ('Marsovin Viticulture Ltd', '2024-05-22T00:00:00.000Z', 'Ortis Ltd', 'Triq L-Industrija, Hal-Qormi', '', '2024-05-22T00:00:00.000Z', '30 days', 'Consumables', null, 575.39, 'No', '2024-07-17T12:08:34.442Z', '2024-07-17T12:08:34.442Z')
+    $db->exec($insert_string);
+}
